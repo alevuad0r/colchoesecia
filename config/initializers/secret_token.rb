734b536edb85675eb10290fd18af0defa68ab266
Alexandre Beserra <ale.beserra@gmail.com>
2011-10-17 22:22:24 -0200

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ortobom::Application.config.secret_token = '616b00567d681b3e1c3f5d80dcecf487ee1cd139a5456cdb83f3b7bedb3af551c85ad0e23423fe42ebd075d21d9da8607eef643ee23f2b1dc7e8185c44034c3b'
