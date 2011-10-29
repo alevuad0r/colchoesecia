f65b566f85b06028da43b640c7bbf6d1731af535
Alexandre Beserra <ale.beserra@gmail.com>
2011-10-29 16:08:18 -0200

    Criando a validacao do cliente

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -44,7 +44,7 @@ class ClientesController < ApplicationController
 
     respond_to do |format|
       if @cliente.save
-        format.html { redirect_to @cliente, notice: 'Cliente was successfully created.' }
+        format.html { redirect_to @cliente, notice: 'Cadastro efetuado com sucesso.' }
         format.json { render json: @cliente, status: :created, location: @cliente }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class ClientesController < ApplicationController
 
     respond_to do |format|
       if @cliente.update_attributes(params[:cliente])
-        format.html { redirect_to @cliente, notice: 'Cliente was successfully updated.' }
+        format.html { redirect_to @cliente, notice: 'Cadastro efetuado com sucesso.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+   def menu_principal
+      menu = %w(cliente orcamento produto venda usuario)
+      menu_principal = "<ul>"
+      menu.each do |item|
+         menu_principal << "<li>" + link_to(item, :controller => item.pluralize) + "</li>"
+      end
+      menu_principal << "</ul>"
+      raw menu_principal
+   end
 end
