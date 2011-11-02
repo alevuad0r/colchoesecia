c95c5ac987b49b50b1f58e12ff2ee1e24c791d8a
Alexandre Beserra <ale.beserra@gmail.com>
2011-11-02 21:43:41 -0200

    Criando botão HOME

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
-   def menu_principal
-      menu = %w(cliente orcamento produto venda usuario)
-      menu_principal = "<ul>"
-      menu.each do |item|
-         menu_principal << "<li>" + link_to(item, :controller => item.pluralize) + "</li>"
-      end
-      menu_principal << "</ul>"
-      raw menu_principal
-   end
+
 end
