8cddbeacf75a4fbeb3db30bc1b29d8c4e54691a2
Arthur Moura Carvalho <armoucar@gmail.com>
2011-11-04 22:06:33 -0200

    criado tabela de relacionamento entre vendas e produtos

--- db/migrate/20111029130316_create_vendas.rb
+++ db/migrate/20111029130316_create_vendas.rb
@@ -2,8 +2,8 @@ class CreateVendas < ActiveRecord::Migration
   def change
     create_table :vendas do |t|
       t.integer :cliente_id
-      t.string :produtos
       t.integer :usuario_id
+      t.string :produtos
       t.float :valor_da_venda
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111104234603) do
+ActiveRecord::Schema.define(:version => 20111105000219) do
 
   create_table "clientes", :force => true do |t|
     t.string   "nome",       :limit => 80
@@ -66,6 +66,11 @@ ActiveRecord::Schema.define(:version => 20111104234603) do
     t.float    "valor_da_venda"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "vendas_produtos", :id => false, :force => true do |t|
+    t.integer "venda_id"
+    t.integer "produto_id"
   end
 
 end
