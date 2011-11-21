5470b154f95cb2143aea747afa814f02b2cc1209
Alexandre Beserra <ale.beserra@gmail.com>
2011-11-21 20:49:22 -0200

    validacoes no model

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,6 +2,7 @@ class Cliente < ActiveRecord::Base
   validates_presence_of :nome, :message=>"deve ser preenchido."
   validates_presence_of :cpf, :message=>"deve ser preenchido."
   validates_presence_of :telefone, :message=>"deve ser preenchido."
+
   validates_uniqueness_of :nome , :message=>"ja existe."
    
   has_many :vendas
