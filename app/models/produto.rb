6725c96c02116494c0086ff6ead66533ffa5fdb7
Alexandre Beserra <ale.beserra@gmail.com>
2011-11-22 21:55:46 -0200

    Finalizando validacoes

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,5 +1,9 @@
 class Produto < ActiveRecord::Base
   
+  validates_presence_of :cod_produto, :message=>"deve ser preenchido."
+  validates_presence_of :nome, :message=>"deve ser preenchido."
+  validates_presence_of :preco, :message=>"deve ser preenchido."
+  
   has_and_belongs_to_many :vendas
   has_and_belongs_to_many :orcamentos
 
