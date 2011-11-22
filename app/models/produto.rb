11b5a8e885bdbeba4bf25e3d7693a5c1bfe8b934
Arthur Moura Carvalho <armoucar@gmail.com>
2011-11-22 21:47:12 -0200

    retirando validacoes do model.

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,11 +1,5 @@
 class Produto < ActiveRecord::Base
   
-  validates_presence_of :cod_produto, :message=> "deve ser preenchido."
-  validates_presence_of :nome, :message=> "deve ser preenchido."
-  validates_presence_of :preco, :message=> "deve ser preenchido."
-  
-  validates_uniqueness_of :cod_produto, :message=> "ja existe."
-  
   has_and_belongs_to_many :vendas
   has_and_belongs_to_many :orcamentos
   
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -7,12 +7,6 @@ class Usuario < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :nome, :telefone, :login, :email, :password, :password_confirmation, :remember_me
   
-  validates_presence_of :nome, :message=> "deve ser preenchido."
-  validates_presence_of :login, :message=> "deve ser preenchido."
-  validates_presence_of :senha, :message=> "deve ser preenchido."
-  
-  validates_uniqueness_of :login, :message=> "ja existe."
-  
   has_many :vendas
   has_many :orcamentos
 
