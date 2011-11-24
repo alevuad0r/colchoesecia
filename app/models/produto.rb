751263154e0fb3cdd57c7e5e8a5674dd5eb89d4c
Arthur Moura Carvalho <armoucar@gmail.com>
2011-11-24 01:08:06 -0200

    continuacao dos ajustes no orcamento

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,5 +2,9 @@ class Produto < ActiveRecord::Base
   
   has_and_belongs_to_many :vendas
   has_and_belongs_to_many :orcamentos
+
+  def to_s
+    nome
+  end
   
 end
--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module Ortobom
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = 'pt-BR'
 
+    config.time_zone = 'Brasilia'
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
